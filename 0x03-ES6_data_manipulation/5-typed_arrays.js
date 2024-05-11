23e14211e0313def2806cdf4a41c2eea2323f8f2
Solomon Bassey <solomonbassey20@gmail.com>
2024-05-11 19:03:58 +0100

    Completed Tasks

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,8 @@
-export default function createInt8TypedArray(length, position, number) {
+export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
     throw new Error('Position outside range');
   }
-  const uint8 = new Uint8Array();
+  const xint8Array = new Int8Array(length);
+  xint8Array.fill(value, position, position + 1);
+  return new DataView(xint8Array.buffer);
 }
