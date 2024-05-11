8ba7a85a42c9eded2a9dd0d592287dc2bc8a3db2
Solomon Bassey <solomonbassey20@gmail.com>
2024-05-11 19:53:31 +0100

    Completed Tasks

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,7 +3,7 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((result) => result.map((status, value, reason) => ({
       status,
       value: status === 'fulfilled' ? value : reason.toString(),
