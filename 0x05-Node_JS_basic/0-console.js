34b6aa25dc55fc9ddf1f4357965b093565e1e6a5
Solomon Bassey <solomonbassey20@gmail.com>
2024-07-07 19:04:20 +0100

    init

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bassey Solomon <https://github.com/Winchestre>
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
+  
+  module.exports = displayMessage;
