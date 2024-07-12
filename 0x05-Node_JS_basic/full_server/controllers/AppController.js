1d3f36e07a65b892b38eb875cd09f4eb58770505
Solomon Bassey <solomonbassey20@gmail.com>
2024-07-13 00:48:05 +0100

    AppController lint fix

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -3,10 +3,10 @@
  * @author Bassey Solomon <https://github.com/Winchestre>
  */
 class AppController {
-    static getHomepage(request, response) {
-      response.status(200).send('Hello Holberton School!');
-    }
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
   }
-  
-  export default AppController;
-  module.exports = AppController;
+}
+
+export default AppController;
+module.exports = AppController;
